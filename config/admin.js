d95e35dc8b660231ecd293cfd9edd1f801f09a5c
Rikesh Shrestha <rikesh.shrestha@ebpearls.com>
2022-06-19 11:54:51 +0545

    updated

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','1g8jW/TqztzEVEGfMHBlZA'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT','v99qSzZRgSl2ZE2YQIq64w'),
